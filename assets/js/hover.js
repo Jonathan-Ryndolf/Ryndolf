36b331ae25edf324eb5c1c8aed35cec7ee9d90ac
Jonathan Ryndolf <ryndolf2001@gmail.com>
2021-05-08 04:37:47 +0530

    designer and developer updated

--- assets/js/hover.js
+++ assets/js/hover.js
@@ -2,11 +2,11 @@ $(function () {
   $("#designer").hover(
     function () {
       $(".big").css("width", "600px");
-      $(".big").css("height", "100px");
+      $(".big").css("height", "300px");
       $(".outline").css("width", "585px");
-      $(".outline").css("height", "85px");
+      $(".outline").css("height", "285px");
       $(".outline").css("border", "2px solid #595959");
-      $(".outline").css("background-color", "#00c5db23");
+      $(".outline").css("background-color", "#00c5db10");
       $(".small").css("background-color", "#00c5db");
     },
     function () {
@@ -25,11 +25,11 @@ $(function () {
 $(function () {
   $("#developer").hover(
     function () {
-      $("#code").css("-webkit-mask-image", "radial-gradient(ellipse 50% 100% at 58% 58%, black 50%, transparent 80%)");
+      $("#code").css("clip-path", "circle(40% at 55% 50%)");
     },
     function () {
       // on mouseout, reset the background colour
-      $("#code").css("-webkit-mask-image", "radial-gradient(ellipse 0% 0% at 58% 58%, black 50%, transparent 80%)");
+      $("#code").css("clip-path", "circle(0% at 55% 50%)");
     }
   );
 });
