5105ca3e0770637aa0601889a1e9033819f4d35a
Jonathan Ryndolf <ryndolf2001@gmail.com>
2021-05-09 00:00:41 +0530

    fixed hovers and added loading animation

--- assets/js/hover.js
+++ assets/js/hover.js
@@ -1,23 +1,31 @@
 $(function () {
   $("#designer").hover(
     function () {
-      $(".big").css("width", "600px");
       $(".big").css("height", "300px");
-      $(".outline").css("width", "585px");
       $(".outline").css("height", "285px");
       $(".outline").css("border", "2px solid #595959");
       $(".outline").css("background-color", "#00c5db10");
-      $(".small").css("background-color", "#00c5db");
+      $(".small").css("opacity", "1");
+      $("#color1, #color2, #color3, #color4, #color5").css("opacity", "1");
+      $("#color1").css("transition-delay", "0s");
+      $("#color2").css("transition-delay", ".25s");
+      $("#color3").css("transition-delay", ".5s");
+      $("#color4").css("transition-delay", ".75s");
+      $("#color5").css("transition-delay", "1s");
     },
     function () {
       // on mouseout, reset the background colour
-      $(".big").css("width", "0px");
       $(".big").css("height", "0px");
-      $(".outline").css("width", "0px");
       $(".outline").css("height", "0px");
       $(".outline").css("border", "0px solid #59595900");
       $(".outline").css("background-color", "#00c5db2300");
-      $(".small").css("background-color", "#00c5db00");
+      $(".small").css("opacity", "0");
+      $("#color1, #color2, #color3, #color4, #color5").css("opacity", "");
+      $("#color1").css("transition-delay", "1s");
+      $("#color2").css("transition-delay", ".75s");
+      $("#color3").css("transition-delay", ".5s");
+      $("#color4").css("transition-delay", ".25s");
+      $("#color5").css("transition-delay", "0s");
     }
   );
 });
@@ -25,17 +33,16 @@ $(function () {
 $(function () {
   $("#developer").hover(
     function () {
-      $("#code").css("clip-path", "circle(40% at 55% 50%)");
-      $("#shadow").css("opacity", "1");
-      $("#shadow").css("transition", "1s");
-      $("#shadow").css("transition-delay", "1s");
+      $("#code").css("clip-path", "circle(30% at 55% 50%)");
     },
     function () {
       // on mouseout, reset the background colour
       $("#code").css("clip-path", "circle(0% at 55% 50%)");
-      $("#shadow").css("opacity", "0");
-      $("#shadow").css("transition", "0s");
-      $("#shadow").css("transition-delay", "0s");
     }
   );
+});
+
+$(window).load(function() {
+  // Animate loader off screen
+  $(".se-pre-con").fadeOut("slow");;
 });
