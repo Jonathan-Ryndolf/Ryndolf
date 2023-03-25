a88ec6c6848cc15a5ca4475c48004c54ee937d87
Jonathan Ryndolf <ryndolf2001@gmail.com>
2023-03-25 20:55:03 +0530

    slider border fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -243,3 +243,38 @@
     items: 1,
   });
 })(jQuery);
+
+const platforms = document.querySelector(".platforms .slide-track");
+
+const rollingAnimation = platforms.animate(
+  [{ transform: "translateX(0)" }, { transform: `translateX(${-200 * 13}px)` }],
+  { iterations: Infinity, duration: 80 * 1000 }
+);
+
+function rollCarousel() {
+  rollingAnimation.play();
+}
+
+function pauseCarousel() {
+  rollingAnimation.pause();
+}
+
+platforms.addEventListener("mouseover", pauseCarousel);
+platforms.addEventListener("mouseout", rollCarousel);
+
+if (window.matchMedia("(max-width: 800px)").matches) {
+  transform: `translateX(${-150 * 13}px)`;
+}
+
+$("a.taphover").on("touchstart", function (e) {
+  "use strict"; //satisfy the code inspectors
+  var link = $(this); //preselect the link
+  if (link.hasClass("hover")) {
+    return true;
+  } else {
+    link.addClass("hover");
+    $("a.taphover").not(this).removeClass("hover");
+    e.preventDefault();
+    return false; //extra, and to make sure the function has consistent return points
+  }
+});
